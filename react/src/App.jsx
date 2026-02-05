6a924fd04a4e9a1b8cb5c209c32804feb96ecb98
a-wotton <a_wotton@fanshaweonline.ca>
2026-02-05 14:11:33 -0500

    set up sass in react app

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './views/Header';
 import Home from './views/home';
 import Gallery from './views/Gallery';
 import FurnitureDetails from './views/FurnitureDetails';
+import '../css/main.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/furniture" element={<Gallery />} />
-          <Route path="furniture-details" element={<FurnitureDetails />} />
+          <Route path="/furniture-details" element={<FurnitureDetails />} />
         </Routes>
       </BrowserRouter>
     </div>
